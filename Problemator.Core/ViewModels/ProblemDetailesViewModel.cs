299c57b15d75abe20ceafb57465c25d05eb97ade
Costin Morariu <cm@interactivemedica.com>
2018-03-22 22:26:29 +0100

    #3 Start adding Problem details page.

--- Problemator.Core/ViewModels/ProblemDetailesViewModel.cs
+++ Problemator.Core/ViewModels/ProblemDetailesViewModel.cs
@@ -2,9 +2,12 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System.ComponentModel;
+using System.Threading.Tasks;
 using MvvmToolkit;
 using MvvmToolkit.Commands;
+using MvvmToolkit.Messages;
 using Problemator.Core.Dtos;
+using Problemator.Core.Models;
 
 namespace Problemator.Core.ViewModels
 {
@@ -16,13 +19,53 @@ namespace Problemator.Core.ViewModels
 
         public bool Busy { get; private set; }
 
-        public int TriesCount { get; set; }
-     
         public WallProblem Problem { get; private set; }
+
+        public ProblemDetails Details { get; private set; }
+
+        private Problem _problem;
+        private IEventAggregator _eventAggregator;
+
+        public ProblemDetailesViewModel(
+            Problem problem,
+            IEventAggregator eventAggregator)
+        {
+            _problem = problem;
+            _eventAggregator = eventAggregator;
+        }
 
         public void Activate(object parameter)
         {
             Problem = (WallProblem) parameter;
+        }
+
+        private RelayCommand _loadComand;
+        public RelayCommand LoadCommand => _loadComand ??
+            (_loadComand = new RelayCommand(async () => await LoadAsync()));
+
+        private async Task LoadAsync()
+        {
+            _eventAggregator.Subscribe(this);
+
+            Busy = true;
+
+            await LoadDetailsAsync();
+
+            Busy = false;
+        }
+
+        private async Task LoadDetailsAsync()
+        {
+            Details = await _problem.GetDetailsAsync(Problem.ProblemId);
+        }
+
+        private RelayCommand _unloadComand;
+        public RelayCommand UnloadCommand => _unloadComand ??
+            (_unloadComand = new RelayCommand(Unload));
+
+        private void Unload()
+        {
+            _eventAggregator.Unsubscribe(this);
         }
     }
 }
